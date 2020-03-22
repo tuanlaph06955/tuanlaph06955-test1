9d38480a0a3c1c70234f5728e1a818c269308361
tuanlaph06955 <tuanlaph06955@fpt.edu.vn>
2020-03-22 16:37:37 +0700

    2 files modified

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -3,15 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductManagerComponent } from './product-manager/product-manager.component';
 import { ListProductComponent } from './list-product/list-product.component';
-import { AboutComponent } from './about/about.coponent';
-import { ServiceComponent } from './service/service.coponent';
-import { HomepageComponent } from './homepage/homepage.coponent';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   { path: 'product-add', component:ProductAddComponent  },
   { path: 'product-manager', component:ProductManagerComponent  },
-  { path: 'list-product', component:ListProductComponent  },
-  { path: 'about', component:ListProductComponent  },
   { path: 'list-product', component:ListProductComponent  },
   { path: 'about', component:AboutComponent  },
   { path: 'service', component:ServiceComponent  },
